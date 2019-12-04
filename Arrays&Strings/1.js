8873156a74bad0752dec8779020ab4da82c54a98
Arun Selva Kumar <b.arunselvakumar@icloud.com>
2019-12-04 09:25:47 +0530

    Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures.

---
+++ Arrays&Strings/1.js
@@ -0,0 +1,30 @@
+/*
+ * Implement an algorithm to determine if a string has all unique characters.
+ * What if you cannot use additional data structures.
+ */
+
+function hasUniqueCharacters(input) {
+    let hasDuplicates = false;
+    const characters = input.split('');
+    const characterDictionary = {};
+
+    characters.forEach(character => {
+        if (characterDictionary[character]) {
+            hasDuplicates = true;
+        }
+
+        characterDictionary[character] = character;
+    });
+
+    return !hasDuplicates;
+}
+
+describe('hasUniqueCharacters', function() {
+    it('should return `TRUE` when the input character is Unique', function () {
+        expect(hasUniqueCharacters('ABCDEFGH')).toBe(true);
+    });
+
+    it('should return `FALSE` when the input character is not Unique', function () {
+        expect(hasUniqueCharacters('ABCDC')).toBe(false);
+    });
+});
